6fa0f933f03b01030a3516ef0ea0ea2c9ee6337f
Cristian Necula <virusz@gmail.com>
2019-06-10 11:41:14 +0300

    (major) import eslint config from cosmoz-frontend

---
+++ test-config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  env: {
+    mocha: true
+  },
+  globals: {
+    a11ySuite: false,
+    assert: false,
+    fixture: false,
+    test: false,
+    sinon: false,
+    chai: false
+  }
+};
