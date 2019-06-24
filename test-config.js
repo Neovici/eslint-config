349779be7e5ee46a50a981765e141e9795c8e366
Cristian Necula <virusz@gmail.com>
2019-06-24 08:14:17 +0300

    (chore) fix linter errors

--- test-config.js
+++ test-config.js
@@ -1,32 +1,32 @@
 module.exports = {
-  parserOptions: {
-    // for async/await test functions
-    ecmaVersion: 2018
-  },
-  env: {
-    mocha: true
-  },
-  plugins: ["mocha"],
-  globals: {
-    // added by mocha
-    test: "readonly",
-    // added by chai
-    chai: "readonly",
-    assert: "readonly",
-    expect: "readonly",
-    // added by test-fixture-mocha
-    fixture: "readonly",
-    // added by sinon
-    sinon: "readonly",
-    // added by wct-mocha
-    WCT: "readonly",
-    flush: "readonly"
-  },
-  rules: {
-    // tests can be as long as they need to be
-    "max-lines-per-function": "off",
-    "max-statements": "off",
-    // for chai expect syntax: `expect().to.be.ok`
-    "no-unused-expressions": "off"
-  }
+	parserOptions: {
+		// for async/await test functions
+		ecmaVersion: 2018
+	},
+	env: {
+		mocha: true
+	},
+	plugins: ['mocha'],
+	globals: {
+		// added by mocha
+		test: 'readonly',
+		// added by chai
+		chai: 'readonly',
+		assert: 'readonly',
+		expect: 'readonly',
+		// added by test-fixture-mocha
+		fixture: 'readonly',
+		// added by sinon
+		sinon: 'readonly',
+		// added by wct-mocha
+		WCT: 'readonly',
+		flush: 'readonly'
+	},
+	rules: {
+		// tests can be as long as they need to be
+		'max-lines-per-function': 'off',
+		'max-statements': 'off',
+		// for chai expect syntax: `expect().to.be.ok`
+		'no-unused-expressions': 'off'
+	}
 };
